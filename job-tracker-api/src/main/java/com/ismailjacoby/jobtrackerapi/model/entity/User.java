e3985b2cb9ca5d3cd777c1de3ac28bf0228e049f
Ismail <ismail.jacoby@gmail.com>
2025-10-21 11:16:41 +0200

    Backend: User Entity & UserRole enum created. Added Security with UserDetailsServiceImpl, JWTProvider, JWTAuthenticationFilter. And created the signup endpoint.

---
+++ job-tracker-api/src/main/java/com/ismailjacoby/jobtrackerapi/model/entity/User.java
@@ -0,0 +1,60 @@
+package com.ismailjacoby.jobtrackerapi.model.entity;
+
+import com.ismailjacoby.jobtrackerapi.model.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User extends BaseEntity<Long> implements UserDetails {
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    private boolean enabled;
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("Role_" + role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
---
+++ job-tracker-api/src/main/java/com/ismailjacoby/jobtrackerapi/model/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.ismailjacoby.jobtrackerapi.model.enums;
+
+public enum UserRole {
+    SUPER_ADMIN,
+    ADMIN,
+    USER
+}
